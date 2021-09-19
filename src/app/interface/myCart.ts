e06926add0af4304e167883d46b4dccc5c49e31a
Naveesh Kumar V <naveesh88@gmail.com>
2021-09-19 11:55:12 +0530

    folder moved to root

--- src/app/interface/myCart.ts
+++
@@ -1,7 +0,0 @@
-import { Product } from "./product";
-
-export class myCart{
-  
-  constructor(public product:Product, public qty:number, public size:string, public price:number){}
-
-}
--- src/app/interface/user.ts
+++
@@ -1,8 +0,0 @@
-import { myCart } from "./myCart";
-
-export interface User{
-  id:number,
-  username:string,
-  password:string,
-  myProducts?:[myCart]
-}
